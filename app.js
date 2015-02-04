c867a309533da310bc1bcda4e50324cf769269b5
Sam David <sfd1987@gmail.com>
2015-02-04 15:55:12 -0800

    adds ng-repeat

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('flapperNews', []);
+
+app.controller('MainCtrl',[
+	'$scope',
+	function($scope){
+		$scope.posts = [
+	  'post 1',
+	  'post 2',
+	  'post 3',
+	  'post 4',
+	  'post 5'
+	  ];
+		$scope.test = "Hello world??!!?";
+	}]);
