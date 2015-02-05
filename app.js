45e7e2429507e6bd725b6c4a81f0bc3fe82a1417
Sam David <sfd1987@gmail.com>
2015-02-04 20:02:14 -0800

    its broken

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
-app.factory('posts',[function(){
+app.factory('posts', [function(){
 	var origin = {
 		post: []
 	};
@@ -17,6 +17,12 @@ app.config([
 				templateUrl: '/home.html',
 				controller: 'MainCtrl'
 			});
+		$stateProvider
+			.state('posts', {
+			  url: '/posts/{id}',
+			  templateUrl: '/posts.html',
+			  controller: 'PostsCtrl'
+			});
 		$urlRouterProvider.otherwise('home');
 }]);
 
@@ -24,7 +30,6 @@ app.controller('MainCtrl',[
 	'$scope',
 	'posts',
 	function($scope, posts){
-		$scope.posts = posts.posts;
 		$scope.posts = [
 		  {title: 'post 1', upvotes: 5},
 		  {title: 'post 2', upvotes: 2},
@@ -35,15 +40,38 @@ app.controller('MainCtrl',[
 		$scope.test = "Hello world??!!?";
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){return;}
-			this.posts.push({
+			$scope.posts.push({
 				title: $scope.title,
 				link: $scope.link,
-				upvotes: 0
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+			    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
 			});
-			this.title = '';
-			this.link = '';
+			$scope.title = '';
+			$scope.link = '';
 		};
 		$scope.incrementUpvotes = function(post) {
 			post.upvotes += 1;
 		};
-	}]);
+		$scope.posts = posts.posts;
+}]);
+
+app.controller('PostsCtrl', [
+	'$scope',
+	'$stateParams',
+	'posts',
+	function($scope, $stateParams, posts){
+		$scope.post = posts.posts[$stateParams.id];
+		$scope.addComment = function(){
+		  if($scope.body === '') { return; }
+		  $scope.post.comments.push({
+		    body: $scope.body,
+		    author: 'user',
+		    upvotes: 0
+		  });
+		  $scope.body = '';
+		};
+
+}]);
