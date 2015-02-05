14c04f637bac208869cbd90ce487431efadb228f
Sam David <sfd1987@gmail.com>
2015-02-04 16:10:21 -0800

    enables getting user input

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ app.controller('MainCtrl',[
 	'$scope',
 	function($scope){
 		$scope.posts = [
-	  'post 1',
-	  'post 2',
-	  'post 3',
-	  'post 4',
-	  'post 5'
-	  ];
+		  {title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 15},
+		  {title: 'post 4', upvotes: 9},
+		  {title: 'post 5', upvotes: 4}
+		];
 		$scope.test = "Hello world??!!?";
+		$scope.addPost = function(){
+			if(!$scope.title || $scope.title === ''){return;}
+			this.posts.push({title: $scope.title, upvotes: 0});
+			this.title = '';
+		}
 	}]);
