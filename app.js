449c2bc7b5150e6a6afb23166935d6626d0e197e
Sam David <sfd1987@gmail.com>
2015-02-05 16:22:42 -0800

    starts posts model with mongoose

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+require('./models/Posts');
+
+mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
