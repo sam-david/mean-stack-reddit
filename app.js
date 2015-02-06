2ce0f991d185ffa1f98b45829367f99f5acac6ac
Sam David <sfd1987@gmail.com>
2015-02-05 16:26:02 -0800

    comments model in place, backend models complete

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 require('./models/Posts');
+require('./models/Comments');
 
 mongoose.connect('mongodb://localhost/news');
 
